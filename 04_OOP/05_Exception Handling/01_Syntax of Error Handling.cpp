9599d922ea109005146cf1997ff214b16d32a425
Muhammad Ismail <mi.ismail000@gmail.com>
2026-06-10 22:22:23 +0500

    add a Exception Handling

---
+++ 04_OOP/05_Exception Handling/01_Syntax of Error Handling.cpp
@@ -0,0 +1,12 @@
+try {
+// Code that may throw an exception
+} 
+catch (ExceptionType1& e1) {
+// Exception handling code for ExceptionType1
+} 
+catch (ExceptionType2& e2) {
+// Exception handling code for ExceptionType2
+} 
+catch (...) {
+// Exception handling code for any other exception
+}
