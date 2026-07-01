1048836b60c8acf8404d2cd1548c9635caf2cd55
Muhammad Ismail <mi.ismail000@gmail.com>
2026-07-01 08:01:13 +0500

    Add a Front Operation of Queue using Linked List

---
+++ 05_Data_Structures/Queue/09_Front Operation Using Linked List.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+class Queue {
+private:
+    Node* front;
+    Node* rear;
+
+public:
+    Queue() {
+        front = rear = NULL;
+    }
+
+    bool isEmpty() {
+        return front == NULL;
+    }
+
+    void enqueue(int value) {
+
+        Node* newNode = new Node(value);
+
+        if (isEmpty())
+            front = rear = newNode;
+        else {
+            rear->next = newNode;
+            rear = newNode;
+        }
+    }
+
+    int getFront() {
+
+        if (isEmpty()) {
+            cout << "Queue is Empty";
+            return -1;
+        }
+
+        return front->data;
+    }
+};
+
+int main() {
+
+    Queue q;
+
+    q.enqueue(100);
+    q.enqueue(200);
+    q.enqueue(300);
+
+    cout << "Front Element: " << q.getFront();
+
+    return 0;
+}
