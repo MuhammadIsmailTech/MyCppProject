a5f8e2bd91b1ffdf487ff64fea8b68e9c5b7ed4f
Muhammad Ismail <mi.ismail000@gmail.com>
2026-07-10 15:12:50 +0500

    Add a complete Program of Ascending Priority Queue Using Array

--- 05_Data_Structures/Queue/14_Complete Implementation of Double Ended Queue (Deque) using Array.cpp
+++ 05_Data_Structures/Queue/14_Complete Implementation of Double Ended Queue (Deque) using Array.cpp
@@ -14,3 +14,70 @@ public:
         rear = 49;
         size = 0;
     }
+
+    void push_back(int x) {
+        if (rear == 99) {
+            cout << "Deque overflow at rear!" << endl;
+            return;
+        }
+        rear++;
+        arr[rear] = x;
+        size++;
+    }
+
+    void push_front(int x) {
+        if (front == 0) {
+            cout << "Deque overflow at front!" << endl;
+            return;
+        }
+        front--;
+        arr[front] = x;
+        size++;
+    }
+ void pop_front() {
+        if (size == 0) {
+            cout << "Deque underflow!" << endl;
+            return;
+        }
+        front++;
+        size--;
+    }
+
+      void pop_back() {
+        if (size == 0) {
+            cout << "Deque underflow!" << endl;
+            return;
+        }
+        rear--;
+        size--;
+    }
+ void print() {
+        if (size == 0) {
+            cout << "Deque is empty!" << endl;
+            return;
+        }
+        for (int i = front; i <= rear; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
+};
+
+int main() {
+    MyDeque dq;
+    
+    dq.push_back(10);
+    dq.push_back(20);
+    dq.push_front(30);
+ 
+    // Print deque elements
+    dq.print();
+    
+    // Pop from front and back
+    dq.pop_front();
+    dq.pop_back();
+    
+    // Print deque elements after pop
+    dq.print();
+
+    return 0;
+}
