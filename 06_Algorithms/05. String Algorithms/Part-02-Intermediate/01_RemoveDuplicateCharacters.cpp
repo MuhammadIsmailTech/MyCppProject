46d737624c6151b43f09550872173631cbeb7467
Muhammad Ismail <mi.ismail000@gmail.com>
2026-08-30 20:06:31 +0500

    01_RemoveDuplicateCharacters.cpp

---
+++ 06_Algorithms/05. String Algorithms/Part-02-Intermediate/01_RemoveDuplicateCharacters.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+string removeDuplicates(string str)
+{
+    unordered_set<char> seen;
+    string result;
+
+    for (char ch : str)
+    {
+        if (seen.find(ch) == seen.end())
+        {
+            seen.insert(ch);
+            result += ch;
+        }
+    }
+
+    return result;
+}
+
+int main()
+{
+    string str;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    cout << "After removing duplicates: "
+         << removeDuplicates(str);
+
+    return 0;
+}
