ddb3a8f6995eec87d0c0f29c2e8b6ceba7352acd
Muhammad Ismail <mi.ismail000@gmail.com>
2026-09-08 20:51:07 +0500

    add a 06_LongestCommonSubsequence.cpp

---
+++ 06_Algorithms/05. String Algorithms/Part-03-Advanced/06_LongestCommonSubsequence.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int LCS(string a, string b)
+{
+    int n = a.length();
+    int m = b.length();
+
+    vector<vector<int>> dp(
+        n + 1,
+        vector<int>(m + 1, 0)
+    );
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (a[i - 1] == b[j - 1])
+            {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else
+            {
+                dp[i][j] =
+                    max(dp[i - 1][j],
+                        dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[n][m];
+}
+
+int main()
+{
+    string a, b;
+
+    cout << "Enter first string: ";
+    cin >> a;
+
+    cout << "Enter second string: ";
+    cin >> b;
+
+    cout << "LCS Length: "
+         << LCS(a, b);
+
+    return 0;
+}
